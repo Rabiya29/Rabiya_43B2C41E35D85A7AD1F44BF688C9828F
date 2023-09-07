99fdf70008203a067171e9af3d1d60a628949584
Rabiya29 <144324890+Rabiya29@users.noreply.github.com>
2023-09-07 13:53:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def fact_rec(n):
+  if n==1 or n==0:
+    return 1
+  else:
+    return   n*fact_rec(n-1) number = int(input("enter the value "))
+res = fact_rec(number)
+
+print("the factorial of",number,"is",res )
